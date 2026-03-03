55a48d5ea6cf95e0b4a8413bf4a650274722f414
c0denaut <nekonaut69@gmail.com>
2026-03-03 22:21:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from k1 import Isotropic
 
-n = int(input("Введите количество точек: "))
-method = Isotropic(n)
+while True:
+    try:
+        n = int(input("Введите количество точек: "))
+        method = Isotropic(n)
+        break
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+        print("Пожалуйста, попробуйте снова.\n")
+
 method.plot()
